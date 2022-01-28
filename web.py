f6e6aceace189dedf37b1e7c2852544ec6497120
Bruno <bru.pava@gmail.com>
2022-01-28 12:58:36 -0300

    more debugs for heroku

--- web.py
+++ web.py
@@ -15,13 +15,6 @@ from spotdl.utils.search import get_search_results
 from spotdl.utils.spotify import SpotifyClient, SpotifyError
 
 
-class App:
-    server: Any
-    downloader: Downloader
-    settings: Dict[str, Any]
-    loop: asyncio.AbstractEventLoop
-
-
 class SongModel(BaseModel):
     """
     A song object used for types and validation.
@@ -50,40 +43,66 @@ class SongModel(BaseModel):
     url: str
     download_url: Optional[str] = None
 
+settings = {
+    'verbose': False,
+    'cache_path': '.',
+    'audio_provider': "youtube-music",
+    'lyrics_provider': "musixmatch",
+    'ffmpeg': "ffmpeg",
+    'variable_bitrate': None,
+    'constant_bitrate': None,
+    'ffmpeg_args': None,
+    'format': "mp3",
+    'save_file': None,
+    'm3u': None,
+    'output': ".",
+    'overwrite': "overwrite",
+    'client_id': '7375acea79274eb2b60280141f60c1c0',
+    'client_secret': '8eb64e22379e45a5ac4e064ed7f1c6ee',
+    'user_auth': False,
+    'threads': 1,
+    'browsers': Optional[List[str]],
+    'progress_handler': None,
+    'no_cache': None,
+}
 
-class SettingsModel(BaseModel):
-    """
-    A settings object used for types and validation.
-    """
+loop = asyncio.new_event_loop()
 
-    verbose: Optional[bool]
-    cache_path: Optional[str]
-    audio_provider: Optional[str]
-    lyrics_provider: Optional[str]
-    ffmpeg: Optional[str]
-    variable_bitrate: Optional[int]
-    constant_bitrate: Optional[int]
-    ffmpeg_args: Optional[List[str]]
-    format: Optional[str]
-    save_file: Optional[str]
-    m3u: Optional[str]
-    output: Optional[str]
-    overwrite: Optional[str]
-    client_id: Optional[str]
-    client_secret: Optional[str]
-    user_auth: Optional[bool]
-    threads: Optional[int]
-    browsers: Optional[List[str]]
-    progress_handler: Optional[str]
+SpotifyClient.init(
+    client_id=settings["client_id"],
+    client_secret=settings["client_secret"],
+    user_auth=settings["user_auth"],
+    cache_path=settings["cache_path"],
+    no_cache=settings["no_cache"],
+)
 
+downloader = Downloader(
+    audio_provider=settings["audio_provider"],
+    lyrics_provider=settings["lyrics_provider"],
+    ffmpeg=settings["ffmpeg"],
+    variable_bitrate=settings["variable_bitrate"],
+    constant_bitrate=settings["constant_bitrate"],
+    ffmpeg_args=settings["ffmpeg_args"],
+    output_format=settings["format"],
+    save_file=settings["save_file"],
+    threads=settings["threads"],
+    output=settings["output"],
+    overwrite=settings["overwrite"],
+    # m3u_file=settings["m3u"],
+    # progress_handler=None,
+    # loop=loop,
+)
 
-# app = App()
-# app.server = FastAPI()
-# app.settings = SettingsModel()
+# config = Config(app=app.server, port=8800, workers=1, loop=loop)  # type: ignore
 
-downloader = GetDownloader()
+# server = Server(config)
+
+# loop.run_until_complete(server.serve())
+
+if downloader.progress_handler:
+    downloader.progress_handler.close()
+
 app = FastAPI()
-
 
 
 @app.get("/api/song/search")
@@ -171,72 +190,3 @@ async def download_objects(
         return FileResponse(path)
 
     return song_obj, path
-
-
-
-def GetDownloader() -> Downloader:
-    """
-    Run the web server.
-    """
-
-    settings = {
-        'verbose': False,
-        'cache_path': '.',
-        'audio_provider': "youtube-music",
-        'lyrics_provider': "musixmatch",
-        'ffmpeg': "ffmpeg",
-        'variable_bitrate': None,
-        'constant_bitrate': None,
-        'ffmpeg_args': None,
-        'format': "mp3",
-        'save_file': None,
-        'm3u': None,
-        'output': ".",
-        'overwrite': "overwrite",
-        'client_id': '7375acea79274eb2b60280141f60c1c0',
-        'client_secret': '8eb64e22379e45a5ac4e064ed7f1c6ee',
-        'user_auth': False,
-        'threads': 1,
-        'browsers': Optional[List[str]],
-        'progress_handler': None,
-        'no_cache': None,
-    }
-
-    loop = asyncio.new_event_loop()
-    loop = loop
-
-    SpotifyClient.init(
-        client_id=settings["client_id"],
-        client_secret=settings["client_secret"],
-        user_auth=settings["user_auth"],
-        cache_path=settings["cache_path"],
-        no_cache=settings["no_cache"],
-    )
-
-    down = Downloader(
-        audio_provider=settings["audio_provider"],
-        lyrics_provider=settings["lyrics_provider"],
-        ffmpeg=settings["ffmpeg"],
-        variable_bitrate=settings["variable_bitrate"],
-        constant_bitrate=settings["constant_bitrate"],
-        ffmpeg_args=settings["ffmpeg_args"],
-        output_format=settings["format"],
-        save_file=settings["save_file"],
-        threads=settings["threads"],
-        output=settings["output"],
-        overwrite=settings["overwrite"],
-        # m3u_file=settings["m3u"],
-        # progress_handler=None,
-        # loop=loop,
-    )
-
-    # config = Config(app=app.server, port=8800, workers=1, loop=loop)  # type: ignore
-
-    # server = Server(config)
-
-    # loop.run_until_complete(server.serve())
-
-    if downloader.progress_handler:
-        downloader.progress_handler.close()
-
-    return down;
